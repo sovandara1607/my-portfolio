66721fcd509b44643c7430c2b0bab355c217fb24
sovandara1607 <srith3@paragoniu.edu.kh>
2026-01-14 22:49:09 +0700

    i dont have a life

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,21 +12,8 @@ export const metadata: Metadata = {
   description: "Year 3 Computer Science Student · Web & Mobile Developer · Aspiring Software Engineer",
   generator: "v0.app",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+    icon: "/profile.PNG",
+    apple: "/profile.PNG",
   },
 }
 
